1b0441ce29e6af6c46efe6451258f9726c57100a
ABHINAV THODISHETTY <143094695+TAbhinav123@users.noreply.github.com>
2025-03-06 12:54:46 +0530

    Update CafeManage.py

--- CafeManage.py
+++ CafeManage.py
@@ -1,31 +1,4 @@
-# menu = {
-#     "PIZZA" : 40,
-#     "Pasta" : 70,
-#     "Burger" : 130,
-#     "Coffee" : 40,
-#     "Juice" : 30
-# }
-# print("Welcome to ABHINAV Restaurent : ")
-# print("Please Select Anys Of The Dishes ")
-# print("PIZZA: Rs40\nPasta: Rs70\nBurger: Rs130\nCoffee: Rs40\nJuice: Rs30 ")
-# order_total = 0
 
-# item_1 = input("Enter the name of item you want to order : ")
-# if item_1 in menu:
-#     order_total += menu[item_1]
-#     print("your item {item_1} has been added to order" )
-# else:
-#     print(f"ordered item {item_1} is not available yet!")
-
-# another_order = input("Do you want to add another item? (Yes/No)")
-# if another_order == "Yes":
-#     item_2 = input("Enter the name of second item = ")
-#     if item_2 in menu:
-#          order_total += menu[item_2]
-#          print(f"your item {item_2} has been added to order" )
-#     else:
-#          print("Ordered item {item_2} is not available")
-# print(f"Total amount of items to pay is {order_total} ")
 menu = {
     "PIZZA" : 40,
     "Pasta" : 70,
